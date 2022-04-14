8e419360c64df7047ccac0a3e8423c4e00987d0f
Sebastiao Fidencio <sfidencio@gmail.com>
2022-04-14 16:38:09 -0300

    Deploy da aplicacao

--- src/main/java/br/com/atendimento/Main.java
+++ src/main/java/br/com/atendimento/Main.java
@@ -2,7 +2,6 @@ package br.com.atendimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Main {
